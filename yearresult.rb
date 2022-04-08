4c59576d5dd01d030417460dc26b8460ba35a615
DenMen94 <88374428+DenMen94@users.noreply.github.com>
2022-04-08 17:44:06 +1000

    Add files via upload
    s

---
+++ yearresult.rb
@@ -0,0 +1,23 @@
+require 'httparty'
+require 'json'
+
+class Scope
+  
+  def initialize 
+
+response = HTTParty.get('https://utmbworld-api.utmb.world/runner/stats/19046/GENERAL')
+JSON.parse(response.body)
+runner = JSON.parse(response.body)
+
+   @year = runner.map{ |year| year["annee"] }
+   @result = runner.map{ |result| result["cote"] }
+
+  end
+
+
+def year_results
+p "year: #{@year}"
+p "results: #{@result}"
+end
+    
+ end
